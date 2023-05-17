0185cb30041a3c57234b6c5dd66ff78618af8731
vamshi krishna <96162476+vamshig736@users.noreply.github.com>
2023-05-17 21:27:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,360 +1,196 @@
 const express = require("express");
-const app = express();
-app.use(express.json());
-const format = require("date-fns/format");
-const isValid = require("date-fns/isValid");
-const toDate = require("date-fns/toDate");
-
+const path = require("path");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const path = require("path");
-const dbPath = path.join(__dirname, "todoApplication.db");
+const app = express();
+app.use(express.json());
 
+const dbPath = path.join(__dirname, "covid19IndiaPortal.db");
 let db = null;
-
-const initializeDBAndServer = async () => {
+const initializeServerDb = async () => {
   try {
     db = await open({
       filename: dbPath,
       driver: sqlite3.Database,
     });
     app.listen(3000, () => {
-      console.log("Server is running at http:/localhost:3000/");
+      console.log("Server is Running http://localhost:3000");
     });
   } catch (e) {
-    console.log(e.message);
+    console.log(`Db Error ${e.message}`);
+    process.exit(1);
   }
 };
-
-initializeDBAndServer();
-
-const checkRequestsQueries = async (request, response, next) => {
-  const { search_q, category, priority, status, date } = request.query;
-  const { todoId } = request.params;
-  if (category !== undefined) {
-    const categoryArray = ["WORK", "HOME", "LEARNING"];
-    const categoryIsInArray = categoryArray.includes(category);
-    if (categoryIsInArray === true) {
-      request.category = category;
-    } else {
-      response.status(400);
-      response.send("Invalid Todo Category");
-      return;
-    }
-  }
-
-  if (priority !== undefined) {
-    const priorityArray = ["HIGH", "MEDIUM", "LOW"];
-    const priorityIsInArray = priorityArray.includes(priority);
-    if (priorityIsInArray === true) {
-      request.priority = priority;
-    } else {
-      response.status(400);
-      response.send("Invalid Todo Priority");
-      return;
-    }
-  }
-
-  if (status !== undefined) {
-    const statusArray = ["TO DO", "IN PROGRESS", "DONE"];
-    const statusIsInArray = statusArray.includes(status);
-    if (statusIsInArray === true) {
-      request.status = status;
-    } else {
-      response.status(400);
-      response.send("Invalid Todo Status");
-      return;
-    }
-  }
-
-  if (date !== undefined) {
-    try {
-      const myDate = new Date(date);
-
-      const formatedDate = format(new Date(date), "yyyy-MM-dd");
-      console.log(formatedDate, "f");
-      const result = toDate(
-        new Date(
-          `${myDate.getFullYear()}-${myDate.getMonth() + 1}-${myDate.getDate()}`
-        )
-      );
-      console.log(result, "r");
-      console.log(new Date(), "new");
-
-      const isValidDate = await isValid(result);
-      console.log(isValidDate, "V");
-      if (isValidDate === true) {
-        request.date = formatedDate;
-      } else {
-        response.status(400);
-        response.send("Invalid Due Date");
-        return;
-      }
-    } catch (e) {
-      response.status(400);
-      response.send("Invalid Due Date");
-      return;
-    }
-  }
-
-  request.todoId = todoId;
-  request.search_q = search_q;
-
-  next();
+initializeServerDb();
+///
+const convertStateObjectsToResponseObjects = (dbObject) => {
+  return {
+    stateId: dbObject.state_id,
+    stateName: dbObject.state_name,
+    population: dbObject.population,
+  };
 };
-
-const checkRequestsBody = (request, response, next) => {
-  const { id, todo, category, priority, status, dueDate } = request.body;
-  const { todoId } = request.params;
-
-  if (category !== undefined) {
-    categoryArray = ["WORK", "HOME", "LEARNING"];
-    categoryIsInArray = categoryArray.includes(category);
-
-    if (categoryIsInArray === true) {
-      request.category = category;
-    } else {
-      response.status(400);
-      response.send("Invalid Todo Category");
-      return;
-    }
-  }
-
-  if (priority !== undefined) {
-    priorityArray = ["HIGH", "MEDIUM", "LOW"];
-    priorityIsInArray = priorityArray.includes(priority);
-    if (priorityIsInArray === true) {
-      request.priority = priority;
-    } else {
-      response.status(400);
-      response.send("Invalid Todo Priority");
-      return;
-    }
-  }
-
-  if (status !== undefined) {
-    statusArray = ["TO DO", "IN PROGRESS", "DONE"];
-    statusIsInArray = statusArray.includes(status);
-    if (statusIsInArray === true) {
-      request.status = status;
-    } else {
-      response.status(400);
-      response.send("Invalid Todo Status");
-      return;
-    }
-  }
-
-  if (dueDate !== undefined) {
-    try {
-      const myDate = new Date(dueDate);
-      const formatedDate = format(new Date(dueDate), "yyyy-MM-dd");
-      console.log(formatedDate);
-      const result = toDate(new Date(formatedDate));
-      const isValidDate = isValid(result);
-      console.log(isValidDate);
-      console.log(isValidDate);
-      if (isValidDate === true) {
-        request.dueDate = formatedDate;
-      } else {
-        response.status(400);
-        response.send("Invalid Due Date");
-        return;
-      }
-    } catch (e) {
-      response.status(400);
-      response.send("Invalid Due Date");
-      return;
-    }
-  }
-  request.todo = todo;
-  request.id = id;
-
-  request.todoId = todoId;
-
-  next();
+const convertDistrictObjectsToResponseObjects = (dbObject) => {
+  return {
+    districtId: dbObject.district_id,
+    districtName: dbObject.district_name,
+    stateId: dbObject.state_id,
+    cases: dbObject.cases,
+    cured: dbObject.cured,
+    active: dbObject.active,
+    deaths: dbObject.deaths,
+  };
 };
-
-//Get Todos API-1
-app.get("/todos/", checkRequestsQueries, async (request, response) => {
-  const { status = "", search_q = "", priority = "", category = "" } = request;
-  console.log(status, search_q, priority, category);
-  const getTodosQuery = `
-        SELECT 
-            id,
-            todo,
-            priority,
-            status,
-            category,
-            due_date AS dueDate 
-        FROM 
-            todo
-        WHERE 
-        todo LIKE '%${search_q}%' AND priority LIKE '%${priority}%' 
-        AND status LIKE '%${status}%' AND category LIKE '%${category}%';`;
-
-  const todosArray = await db.all(getTodosQuery);
-  response.send(todosArray);
-});
-
-//GET Todo API-2
-app.get("/todos/:todoId", checkRequestsQueries, async (request, response) => {
-  const { todoId } = request;
-
-  const getTodosQuery = `
-        SELECT 
-            id,
-            todo,
-            priority,
-            status,
-            category,
-            due_date AS dueDate
-        FROM 
-            todo            
-        WHERE 
-            id = ${todoId};`;
-
-  const todo = await db.get(getTodosQuery);
-  response.send(todo);
-});
-
-//GET Agenda API-3
-app.get("/agenda/", checkRequestsQueries, async (request, response) => {
-  const { date } = request;
-  console.log(date, "a");
-
-  const selectDuaDateQuery = `
-        SELECT
-            id,
-            todo,
-            priority,
-            status,
-            category,
-            due_date AS dueDate
-        FROM 
-            todo
-        WHERE 
-            due_date = '${date}'
-        ;`;
-
-  const todosArray = await db.all(selectDuaDateQuery);
-
-  if (todosArray === undefined) {
-    response.status(400);
-    response.send("Invalid Due Date");
+function authenticateToken(request, response, next) {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401);
+    response.send("Invalid JWT Token");
   } else {
-    response.send(todosArray);
+    jwt.verify(jwtToken, "MY_SECRET_TOKEN", async (error, payload) => {
+      if (error) {
+        response.status(401);
+        response.send("Invalid JWT Token");
+      } else {
+        next();
+      }
+    });
+  }
+}
+
+app.post("/login/", async (request, response) => {
+  let { username, password } = request.body;
+  let checkTheUsername = `
+            SELECT *
+            FROM user
+            WHERE username = '${username}';`;
+  let userData = await db.get(checkTheUsername);
+  if (userData === undefined) {
+    response.status(400);
+    response.send("Invalid user");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, userData.password);
+    if (isPasswordMatched === true) {
+      const payload = { username: username };
+      const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
+      response.send({ jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid password");
+    }
   }
 });
-
-//Add Todo API-4
-app.post("/todos/", checkRequestsBody, async (request, response) => {
-  const { id, todo, category, priority, status, dueDate } = request;
-
-  const addTodoQuery = `
-        INSERT INTO 
-            todo (id, todo, priority, status, category, due_date)
-        VALUES
-            (
-                ${id},
-               '${todo}',
-               '${priority}',
-               '${status}',
-               '${category}',
-               '${dueDate}'
-            )
-        ;`;
-
-  const createUser = await db.run(addTodoQuery);
-  console.log(createUser);
-  response.send("Todo Successfully Added");
+//
+app.get("/states/", authenticateToken, async (request, response) => {
+  const getstatesQuery = `
+  SELECT * FROM state;`;
+  const statesArray = await db.all(getstatesQuery);
+  response.send(
+    statesArray.map((eachArray) =>
+      convertStateObjectsToResponseObjects(eachArray)
+    )
+  );
 });
-
-//Update Todo API-5
-app.put("/todos/:todoId/", checkRequestsBody, async (request, response) => {
-  const { todoId } = request;
-
-  const { priority, todo, status, category, dueDate } = request;
-
-  let updateTodoQuery = null;
-
-  console.log(priority, todo, status, dueDate, category);
-  switch (true) {
-    case status !== undefined:
-      updateTodoQuery = `
-            UPDATE
-                todo
-            SET 
-                status = '${status}'
-            WHERE 
-                id = ${todoId}     
-        ;`;
-      await db.run(updateTodoQuery);
-      response.send("Status Updated");
-      break;
-    case priority !== undefined:
-      updateTodoQuery = `
-            UPDATE
-                todo
-            SET 
-                priority = '${priority}'
-            WHERE 
-                id = ${todoId}     
-        ;`;
-      await db.run(updateTodoQuery);
-      response.send("Priority Updated");
-      break;
-    case todo !== undefined:
-      updateTodoQuery = `
-            UPDATE
-                todo
-            SET 
-                todo = '${todo}'
-            WHERE 
-                id = ${todoId}     
-        ;`;
-      await db.run(updateTodoQuery);
-      response.send("Todo Updated");
-      break;
-    case category !== undefined:
-      const updateCategoryQuery = `
-            UPDATE
-                todo
-            SET 
-                category = '${category}'
-            WHERE 
-                id = ${todoId}     
-        ;`;
-      await db.run(updateCategoryQuery);
-      response.send("Category Updated");
-      break;
-    case dueDate !== undefined:
-      const updateDateQuery = `
-            UPDATE
-                todo
-            SET 
-                due_date = '${dueDate}'
-            WHERE 
-                id = ${todoId}     
-        ;`;
-      await db.run(updateDateQuery);
-      response.send("Due Date Updated");
-      break;
+////
+app.get("/states/:stateId/", authenticateToken, async (request, response) => {
+  const { stateId } = request.params;
+  const getStateQuery = `
+SELECT * FROM state WHERE state_id=${stateId}`;
+  const state = await db.get(getStateQuery);
+  response.send(convertStateObjectsToResponseObjects(state));
+});
+///
+app.get(
+  "/districts/:districtId/",
+  authenticateToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+    const getDistrictQuery = `
+SELECT * FROM district WHERE district_id=${districtId}`;
+    const district = await db.get(getDistrictQuery);
+    response.send(convertDistrictObjectsToResponseObjects(district));
   }
+);
+///
+app.post("/districts/", authenticateToken, async (request, response) => {
+  const { districtName, stateId, cases, cured, active, deaths } = request.body;
+  const postDistrictQuery = `
+INSERT INTO district (district_name,state_id,cases,cured,active,deaths)
+VALUES (
+    '${districtName}',
+    ${stateId},
+    ${cases},
+    ${cured},
+    ${active},
+    ${deaths}
+)`;
+  await db.run(postDistrictQuery);
+  response.send("District Successfully Added");
 });
 
-//Delete Todo API-6
-app.delete("/todos/:todoId/", async (request, response) => {
-  const { todoId } = request.params;
-  const deleteTodoQuery = `
-            DELETE FROM 
-                todo
-            WHERE 
-               id=${todoId}
-     ;`;
-
-  await db.run(deleteTodoQuery);
-  response.send("Todo Deleted");
-});
-
+app.delete(
+  "/districts/:districtId/",
+  authenticateToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+    const deleteQuery = `
+    DELETE FROM district WHERE district_id=${districtId}`;
+    await db.run(deleteQuery);
+    response.send("District Removed");
+  }
+);
+///
+app.put(
+  "/districts/:districtId/",
+  authenticateToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+    const {
+      districtName,
+      stateId,
+      cases,
+      cured,
+      active,
+      deaths,
+    } = request.body;
+    const updateDistrictQuery = `
+    UPDATE district
+    SET 
+    district_name='${districtName}',
+    state_id=${stateId},
+    cases=${cases},
+    cured=${cured},
+    active=${active},
+    deaths=${deaths}
+    WHERE district_id=${districtId}`;
+    const dbresponse = await db.run(updateDistrictQuery);
+    response.send("District Details Updated");
+  }
+);
+///
+app.get(
+  "/states/:stateId/stats/",
+  authenticateToken,
+  async (request, response) => {
+    const { stateId } = request.params;
+    const getStateStatesQuery = `
+    SELECT 
+    SUM(cases) as totalCases,
+    SUM(cured) as totalCured,
+    SUM(active) as totalActive,
+    SUM(deaths) as totalDeaths
+    FROM 
+    district
+    WHERE state_id=${stateId}`;
+    const state = await db.get(getStateStatesQuery);
+    response.send(state);
+  }
+);
 module.exports = app;
